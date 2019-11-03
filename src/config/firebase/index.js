778d9fc731b9ab39ee7d68fc6900307d94fd6eaa
Danis Zaidan <daniszaidan@gmail.com>
2019-11-03 19:36:53 +0700

    Add Firebase to Project

---
+++ src/config/firebase/index.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+// import 'firebase/firestore';
+// import 'firebase/auth';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBn-LzMqfucscxRSIf6JsCnnS67B2WUmAA",
+    authDomain: "simple-notes-a7ae1.firebaseapp.com",
+    databaseURL: "https://simple-notes-a7ae1.firebaseio.com",
+    projectId: "simple-notes-a7ae1",
+    storageBucket: "simple-notes-a7ae1.appspot.com",
+    messagingSenderId: "262537424468",
+    appId: "1:262537424468:web:070c7a4195c6b1243207c1"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
